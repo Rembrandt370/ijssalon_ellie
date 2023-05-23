47f1227b8c38dca3e737401a2235759915a8c7da
rembrandt <rembrandt82@gmail.com>
2023-05-23 12:41:35 +0200

    laatst toegevoegd

--- algemene_functies.py
+++ algemene_functies.py
@@ -1,3 +1,42 @@
-def mijn_functie_1():
-    
+def mijn_functie_1(argument):
+        retourwaarden = {
+             2: 4,
+             4: 16,
+             10: 100,
+             12: 144
+        }
 
+        if argument in retourwaarden:
+             return retourwaarden[argument]
+        
+waarden1 = mijn_functie_1(2)
+print(waarden1)
+waarden2 = mijn_functie_1(4)
+print(waarden2)
+waarden3 = mijn_functie_1(10)
+print(waarden3)
+waarden4 = mijn_functie_1(12)
+print(waarden4)
+
+
+def mijn_functie_2(argument):
+        retourwaarden = {
+             12.3: [15, 9, 36, 4],
+             12.2: [14, 10, 24, 6],
+             10.5: [15, 5, 50, 2],
+             100.20: [120, 80, 2000, 5]
+        }
+
+        if argument in retourwaarden:
+             return retourwaarden[argument]
+waarden1 = mijn_functie_2(12.3)
+print(waarden2)
+waarden2 = mijn_functie_2(12.2)
+print(waarden2)
+waarden3 = mijn_functie_2(10.5)
+print(waarden3)
+waarden4 = mijn_functie_2(100.20)
+print(waarden4)        
+
+
+        
---
+++ reclame.py
@@ -0,0 +1,72 @@
+def aanbiedingen(smaak, prijs, korting):
+    aanbieding = f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de {smaak}, van {prijs} euro voor {prijs - (prijs * korting)} euro."
+    return aanbieding
+
+resultaat = aanbiedingen("aardbei", 4, 0.1)
+print(resultaat)
+
+
+def inkomsten_totaal(btw_percentage):
+    inkomsten = [220, 430, 125, 160, 260, 205, 90, 345]
+    totaal_inkomsten = sum(inkomsten)
+    btw_bedrag = totaal_inkomsten * btw_percentage
+    totaal_inclusief_btw = totaal_inkomsten + btw_bedrag
+    return totaal_inclusief_btw, btw_bedrag
+
+btw_percentage = 0.09
+resultaat, btw_bedrag = inkomsten_totaal(btw_percentage)
+
+resultaat_string = f"Het totaal van alle inkomsten van deze week is {resultaat} euro, waarover {btw_bedrag} euro btw betaald dient te worden."
+print(resultaat_string)
+
+def laag_en_hoog():
+    mijn_lijst = [220, 430, 125, 160, 205, 90, 345]
+    hoogste_waarde = max(mijn_lijst)
+    laagste_waarde = min(mijn_lijst)
+    print("Mijn hoogste waarden is:", hoogste_waarde)
+    print("Mijn laagste waarden is:", laagste_waarde)
+laag_en_hoog()
+
+def gemiddelde():
+    mijn_lijst = [220, 430, 125, 160, 205, 90, 345]
+    totaal = sum(mijn_lijst)
+    aantal_elementen = len(mijn_lijst)
+    gemiddelde = totaal / aantal_elementen
+    return gemiddelde
+resultaat = gemiddelde()
+print("Het gemiddelde is:", resultaat)
+
+def gemiddelde():
+    mijn_lijst = [220, 430, 125, 160, 205, 90, 345]
+    totaal = sum(mijn_lijst)
+    return totaal
+
+resultaat = gemiddelde()
+resultaat_string = f"De gemiddelde inkomsten deze week zijn {resultaat} euro."
+print(resultaat_string)
+
+def meervoudig():
+    invoer_lijst = [10,5,3,2,1,2,9]
+    laagste_waarden = max(invoer_lijst)
+    laagste_waarden = min(invoer_lijst)
+    laag_en_hoog()
+meervoudig()
+
+
+def combinatie(invoer_lijst_2):
+    return meervoudig()
+resultaat = meervoudig()
+combinatie_resultaat = combinatie(resultaat)
+
+    
+    
+
+
+
+
+
+
+
+
+
+
