38eb9a6058f648bdd3153851d67bf2ed246b7d8d
rembrandt <rembrandt82@gmail.com>
2023-05-24 16:22:24 +0200

    update

--- reclame.py
+++ reclame.py
@@ -58,7 +58,10 @@ def combinatie(invoer_lijst_2):
 resultaat = meervoudig()
 combinatie_resultaat = combinatie(resultaat)
 
-    
+def combinatie(invoer_lijst_2):
+    korte_lijst = laag_en_hoog(invoer_lijst_2)
+    uitvoer = mijn_functie_2(korte_lijst[0], korte_lijst[1])
+    return uitvoer   
     
 
 
